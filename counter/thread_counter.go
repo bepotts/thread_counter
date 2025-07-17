6a6ba49f99006701874fff3463e324f4c7063434
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-07-17 17:55:27 -0400

    added some comments

--- counter/thread_counter.go
+++ counter/thread_counter.go
@@ -1,17 +1,23 @@
 package counter
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 var waitGroup sync.WaitGroup
 
+// NonThreadedCount performs a group of counts using a single thread
 func NonThreadedCount(countTarget int) {
 	for k := 0; k < 4; k++ {
 		for i := 0; i < countTarget; i++ {
 			// do nothing
 		}
+		fmt.Println("Completed one non-threaded count")
 	}
 }
 
+// ThreadedCount performs a group of counts using goroutines
 func ThreadedCount(countTarget int) {
 	for i := 0; i < 4; i++ {
 		waitGroup.Add(1)
@@ -20,9 +26,11 @@ func ThreadedCount(countTarget int) {
 	waitGroup.Wait()
 }
 
+// incrementally counts up to a given number
 func increment(targetCount int) {
 	defer waitGroup.Done()
 	for i := 0; i < targetCount; i++ {
 		// do nothing
 	}
+	fmt.Println("Completed one threaded count")
 }
