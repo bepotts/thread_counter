2d7c8dc76daf8ec49177fd9399360602ef2e1a6f
Brandon Potts <ObeliskIsHuge@gmail.com>
2025-07-17 12:00:08 -0400

    completed non-threaded count

--- counter/thread_counter.go
+++ counter/thread_counter.go
@@ -1,1 +1,20 @@
 package counter
+
+import (
+	"fmt"
+)
+
+var threadedCount = 0
+
+func NonThreadedCount(countTarget int) {
+	var nonThreadedCount = 0
+	for i := 0; i < countTarget; i++ {
+		nonThreadedCount++
+	}
+}
+
+func ThreadedCount(countTarget int) {
+
+	fmt.Println("Starting threaded count")
+
+}
